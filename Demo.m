83c9aca160b9cb38dda07b81c8371048053ceec3
zyao2 <zhengshengy@yahoo.com>
2020-06-20 10:17:15 -0600

    Update Demo.m

--- Demo.m
+++ Demo.m
@@ -1,5 +1,6 @@
 %Single shot RTM;
 %velocity model
+clear all
 nz=81;nx=201;
 vel=zeros(nz,nx);
 vel(1:30,:)=1000;
@@ -8,11 +9,12 @@ vel(61:end,:)=1500;
 
 %source and receiver location;
 dx=5;dz=5;
-sx=100*dx;xz=0;
-recx=(0:2:(nx-1))*dx; recz=zeros(size(gx));
-
+sx=100*dx;sz=0;
+x = (0:nx-1)*dx; z = (0:nz-1)*dx;
+recx=(0:2:(nx-1))*dx; recz=zeros(size(recx));
+nrec=length(recx);
 %FD parameters;
-nbc=20; nt=2001; dt=0.0005;
+nbc=20; nt=2001; dt=0.0005; t=(0:nt-1)*dt;
 
 %source wavelet;
 freq=25; s=ricker(freq,dt);
@@ -29,13 +31,13 @@ load seis
 
 figure(1);set(gcf,'position',[0 0 1000 400]);subplot(231);imagesc(x,z,vel);colorbar;
 xlabel('X (m)'); ylabel('Z (m)'); title('velocity');
-figure(1); subplot(232);imagesc(g,t,seis);colormap(gray);
-title('Seismic Profile');ylabel('Time (s)');xlabel('g #');caxis([-0.25 0.25]);
+figure(1); subplot(232);imagesc(1:nrec,t,seis);colormap(gray);
+title('Seismic Profile');ylabel('Time (s)');xlabel('rec #');caxis([-0.25 0.25]);
 
 %Run the rtm code, watch the movie of forward propagation field, reconstruction forward field, backward propagation field and accumulated rtm image;
 
 tic;
-[img,illum]=rtm(seis,vel_ss,nbc,dx,nt,dt,s,sx,sz,gx,gz);
+[img,illum]=rtm(seis,vel_ss,nbc,dx,nt,dt,s,sx,sz,recx,recz);
 toc;
 
 %Plot the illumination compensation, and see the impact.
@@ -43,8 +45,9 @@ toc;
 figure(2); set(gcf,'position',[0 0 400 600]);colormap(gray);
 subplot(311);imagesc(x,z,img);caxis([-10 10]);
 xlabel('X (m)'); ylabel('Z (m)'); title('rtm image');
-figure(4);subplot(312);imagesc(x,z,illum);caxis([-100 100]);
+figure(2);subplot(312);imagesc(x,z,illum);caxis([-100 100]);
 xlabel('X (m)'); ylabel('Z (m)'); title('illumination compensation');
-figure(4);subplot(313);imagesc(x,z,img./illum);caxis([-1 1]);
+figure(2);subplot(313);imagesc(x,z,img./illum);caxis([-1 1]);
 xlabel('X (m)'); ylabel('Z (m)'); title('rtm image after compensation');
+
 
